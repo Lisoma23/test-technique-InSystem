6e9f2a8ce76df90f18594f70dd41882faf59ba47
Sofia <sf.hazami@gmail.com>
2025-07-20 19:29:40 +0200

    Test Tanstack

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
+import Example from './Example';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Example/>
       </header>
     </div>
   );
 }
-
