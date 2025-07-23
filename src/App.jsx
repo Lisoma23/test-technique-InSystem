47c2122886af40212408d4537c6439e931f7e950
Sofia <sf.hazami@gmail.com>
2025-07-23 23:40:34 +0200

    stockage région et station

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import "./App.css";
 import API from "./HubeauAPI";
 import GraphEtData from "./GraphEtData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [codeStation, setCodeStation] = useState("");
-  const [regionStation, setRegionStation] = useState("");
+    // Initialiser les valeurs depuis localStorage si présentes
+  const [codeStation, setCodeStation] = useState(() => {
+    return localStorage.getItem("codeStation") || "";
+  });
+  const [regionStation, setRegionStation] = useState(() => {
+    return localStorage.getItem("regionStation") || "";
+  });
+
+  // Mettre à jour le localStorage à chaque changement
+  useEffect(() => {
+    if (codeStation) {
+      localStorage.setItem("codeStation", codeStation);
+    }
+  }, [codeStation]);
+
+  useEffect(() => {
+    if (regionStation) {
+      localStorage.setItem("regionStation", regionStation);
+    }
+  }, [regionStation]);
 
   return (
     <div className="App">
