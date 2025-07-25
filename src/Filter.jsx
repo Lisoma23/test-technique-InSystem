a20ee3b5ba3f899d8264b3dcace625e3fa70dd43
Sofia <115009629+Lisoma23@users.noreply.github.com>
2025-07-25 00:24:35 +0000

    remove filters inutiles

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -2,55 +2,14 @@ import DebouncedInput from "./DebouncedInput";
 
 export default function Filter({ column }) {
   const columnFilterValue = column.getFilterValue();
-  const filterVariant = column.columnDef.meta?.filterVariant;
 
-  if (filterVariant === "range") {
-    return (
-      <div>
-        <div className="flex space-x-2">
-          <DebouncedInput
-            type="number"
-            value={(columnFilterValue && columnFilterValue[0]) ?? ""}
-            onChange={(value) =>
-              column.setFilterValue((old) => [value, old?.[1]])
-            }
-            placeholder="Min"
-            className="w-24 border shadow rounded"
-          />
-          <DebouncedInput
-            type="number"
-            value={(columnFilterValue && columnFilterValue[1]) ?? ""}
-            onChange={(value) =>
-              column.setFilterValue((old) => [old?.[0], value])
-            }
-            placeholder="Max"
-            className="w-24 border shadow rounded"
-          />
-        </div>
-        <div className="h-1" />
-      </div>
-    );
-  } else if (filterVariant === "select") {
-    return (
-      <select
-        onChange={(e) => column.setFilterValue(e.target.value)}
-        value={columnFilterValue ? columnFilterValue.toString() : ""}
-      >
-        <option value="">All</option>
-        <option value="complicated">complicated</option>
-        <option value="relationship">relationship</option>
-        <option value="single">single</option>
-      </select>
-    );
-  } else {
-    return (
-      <DebouncedInput
-        className="w-36 border shadow rounded"
-        onChange={(value) => column.setFilterValue(value)}
-        placeholder="Search..."
-        type="text"
-        value={columnFilterValue ?? ""}
-      />
-    );
-  }
+  return (
+    <DebouncedInput
+      className="border shadow rounded text-[1.8vw] text-center w-full"
+      onChange={(value) => column.setFilterValue(value)}
+      placeholder="Search..."
+      type="text"
+      value={columnFilterValue ?? ""}
+    />
+  );
 }
